ba8466aa6bd2062853f4cbac222e06d4fcc27edb
Fábio Naspolini <fabionaspolini@gmail.com>
2024-02-15 16:28:34 -0300

    wsl aliases

--- bash/.wsl_aliases.win.sh
+++ bash/.wsl_aliases.win.sh
@@ -1,2 +1,2 @@
-alias start-docker="wsl -d Ubuntu-20.04 sudo service docker start"
+alias start-docker="wsl -d Ubuntu-20.04 -- sudo service docker start"
 alias stop-wsl="wsl --shutdown Ubuntu-20.04"
