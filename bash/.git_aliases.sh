b6d1adb2876331701223c613fffc42e469c49e96
Fábio Naspolini <fabionaspolini@gmail.com>
2024-08-13 20:11:56 -0300

    git

--- bash/.git_aliases.sh
+++ bash/.git_aliases.sh
@@ -1,4 +1,5 @@
 alias gf="git fetch --prune"
 alias gp="git pull --prune"
 alias gr="git remote -v"
+alias gs="git status"
 alias git-get-all-branchs='git branch -r | grep -v "\->" | while read remote; do git branch --track "${remote#origin/}" "$remote"; done'
