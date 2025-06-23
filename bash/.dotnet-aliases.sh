fcecf50790d8a33e1c0e5c015d6ffe62ca6bb3f8
Fábio Naspolini <fabionaspolini@gmail.com>
2025-06-23 00:32:16 -0300

    git bash configs

--- bash/.dotnet-aliases.sh
+++ bash/.dotnet-aliases.sh
@@ -1,1 +1,6 @@
 alias dotnet-clear-nuget-cache="dotnet nuget locals --clear all"
+
+# Path
+if [ -d ~/.dotnet/tools ]; then
+    export PATH="$PATH:~/.dotnet/tools"
+fi
--- bash/.profile-init.sh
+++ bash/.profile-init.sh
@@ -4,3 +4,14 @@
 . ~/.dev-settings/bash/.git-aliases.sh
 . ~/.dev-settings/bash/.terraform-aliases.sh
 . ~/.dev-settings/bash/scripts/.aliases.sh
+
+# Enable Git branch display in prompt
+if [ -f /usr/lib/git-core/git-sh-prompt ]; then
+    . /usr/lib/git-core/git-sh-prompt
+fi
+export GIT_PS1_SHOWDIRTYSTATE=true
+export GIT_PS1_SHOWUNTRACKEDFILES=true
+export GIT_PS1_SHOWUPSTREAM="auto"
+export GIT_PS1_SHOWCONFLICTSTATE="yes"
+export GIT_PS1_SHOWCOLORHINTS=true
+export PS1='\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]$(__git_ps1 " (%s)")\$ '
