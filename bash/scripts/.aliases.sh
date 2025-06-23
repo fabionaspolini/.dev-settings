4a66c1a608a86ab2ea732f2cd816c93dc1c5d7ff
Fábio Naspolini <fabionaspolini@gmail.com>
2025-06-23 00:58:38 -0300

    limpar variáveis do escopo externo

--- bash/scripts/.aliases.sh
+++ bash/scripts/.aliases.sh
@@ -1,3 +1,5 @@
-declare DEV_SETTINGS_SCRIPTS_DIR="$(dirname -- "${BASH_SOURCE[0]}")"
+DEV_SETTINGS_SCRIPTS_DIR="$(dirname -- "${BASH_SOURCE[0]}")"
 
 alias clear-dev-folders=". $DEV_SETTINGS_SCRIPTS_DIR/clear-dev-folders.sh"
+
+unset DEV_SETTINGS_SCRIPTS_DIR
