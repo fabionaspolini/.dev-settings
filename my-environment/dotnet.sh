a6159417315b8c1613a59f4d0cdee7ac77b96015
Fábio Naspolini <fabionaspolini@gmail.com>
2025-08-16 23:23:56 -0300

    feat(bash): adicionar conclusão de parâmetros para o CLI do dotnet

--- my-environment/dotnet.sh
+++ my-environment/dotnet.sh
@@ -10,3 +10,16 @@ export DOTNET_CLI_UI_LANGUAGE=en
 export AcceptAndroidSDKLicenses=True
 export AndroidSdkDirectory=/home/Android/Sdk/
 export JavaSdkDirectory=/lib/jvm/jdk-21.0.8+9/
+
+# bash parameter completion for the dotnet CLI
+function _dotnet_bash_complete()
+{
+  local cur="${COMP_WORDS[COMP_CWORD]}" IFS=$'\n' # On Windows you may need to use use IFS=$'\r\n'
+  local candidates
+
+  read -d '' -ra candidates < <(dotnet complete --position "${COMP_POINT}" "${COMP_LINE}" 2>/dev/null)
+
+  read -d '' -ra COMPREPLY < <(compgen -W "${candidates[*]:-}" -- "$cur")
+}
+
+complete -f -F _dotnet_bash_complete dotnet
