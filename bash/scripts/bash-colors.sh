a7a37a06d38eca085d59dd5cf00501c1521cd905
Fábio Naspolini <fabionaspolini@gmail.com>
2026-03-14 21:41:51 -0300

    feat(bash): adicionar demonstração de cores no terminal
    Este script demonstra as combinações de cores de fundo e texto no terminal usando códigos ANSI.
    Ele itera sobre os códigos de fundo (background) de 40 a 47 e de texto (foreground) de 30 a 37,
    imprimindo uma amostra de texto colorido para cada combinação possível.

--- bash/scripts/bash-colors.sh
+++ bash/scripts/bash-colors.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+# Este script demonstra as combinações de cores de fundo e texto no terminal usando códigos ANSI.
+# Ele itera sobre os códigos de fundo (background) de 40 a 47 e de texto (foreground) de 30 a 37,
+# imprimindo uma amostra de texto colorido para cada combinação possível.
+
 for bg in {40..47}; do
   for fg in {30..37}; do
     echo -e "\e[${fg};${bg}m fg=${fg}, bg=${bg} \e[0m"
