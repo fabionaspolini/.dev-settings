fc80124b0d952eabc7d4cc775587b37eff134efc
Fábio Naspolini <fabionaspolini@gmail.com>
2026-04-23 14:13:08 -0300

    create refresh-envs-so.sh script

--- bash/scripts/refresh-envs-so.sh
+++ bash/scripts/refresh-envs-so.sh
@@ -6,7 +6,10 @@ source "$HOME/.profile"
 source "$HOME/.bashrc"
 source "$HOME/.bash_profile"
 
-# Sincroniza o ambiente do D-Bus com o do Systemd
+# Atualizar o ambiente do Systemd (User)
+systemctl --user import-environment
+
+# Atualizar o ambiente do D-Bus. Para que aplicativos gráficos (que geralmente são lançados via D-Bus) reconheçam a mudança.
 dbus-update-activation-environment --systemd --all
 
 # Apenas uma variável (ou lista)
