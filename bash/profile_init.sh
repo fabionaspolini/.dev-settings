18efc4a8388197192837f6aed93cf2de2013991f
Fábio Naspolini <fabionaspolini@gmail.com>
2026-04-30 20:58:50 -0300

    kafka script autocomplete

--- bash/profile_init.sh
+++ bash/profile_init.sh
@@ -2,3 +2,8 @@ DEV_TOOLS_DIR="$(dirname -- "${BASH_SOURCE[0]}")"
 
 . "$DEV_TOOLS_DIR"/aliases/init.sh
 . "$DEV_TOOLS_DIR"/scripts/aliases.sh
+
+# Register autocomplete for kafka.sh
+_KAFKA_SOURCED_FOR_INIT=1 . "$DEV_TOOLS_DIR"/scripts/kafka.sh
+register_kafka_autocomplete
+unset _KAFKA_SOURCED_FOR_INIT
--- bash/scripts/kafka.sh
+++ bash/scripts/kafka.sh
@@ -1,3 +1,31 @@
+# Enable autocomplete for this script
+_kafka_autocomplete() {
+  local cur=${COMP_WORDS[COMP_CWORD]}
+  local opcoes="start stop"
+  
+  # compgen gera a lista filtrando pelo que já foi digitado
+  COMPREPLY=( $(compgen -W "${opcoes}" -- "$cur") )
+}
+
+register_kafka_autocomplete() {
+  complete -F _kafka_autocomplete kafka
+  complete -F _kafka_autocomplete kafka.sh
+  complete -F _kafka_autocomplete ./kafka.sh
+}
+
+handle_kafka_command() {
+  case "$1" in
+      start)
+          start
+          ;;
+      stop)
+        stop
+        ;;
+      *)
+          printf "Invalid command: $@\nUse: {start|stop}\n"
+  esac
+}
+
 start() {
   docker compose -f ~/sources/infra/kafka/docker-compose.yml start
 }
@@ -6,16 +34,6 @@ stop() {
   docker compose -f ~/sources/infra/kafka/docker-compose.yml stop
 }
 
-# Esta linha é essencial para permitir a chamada externa
-#"$@"
-
-case "$1" in
-    start)
-        start
-        ;;
-    stop)
-      stop
-      ;;
-    *)
-        printf "Invalid command: $@\nUse: {start|stop}\n"
-esac
+if [[ -z "$_KAFKA_SOURCED_FOR_INIT" ]]; then
+  handle_kafka_command "$@"
+fi
