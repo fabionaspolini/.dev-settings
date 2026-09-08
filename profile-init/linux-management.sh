704506fa59482aebb4910cdd0d22332e5e9836b5
Fábio Naspolini <fabionaspolini@gmail.com>
2026-09-08 18:43:00 -0300

    split upgrade-all-packages to new file

--- profile-init/linux-management.sh
+++ profile-init/linux-management.sh
@@ -1,55 +1,5 @@
 alias fix-keyboard-ibus-errors="ibus restart"
 
-CURRENT_FOLDER="$(dirname -- "${BASH_SOURCE[0]}")"
-source "$CURRENT_FOLDER/../scripts/utils/parse-args.sh"
-unset CURRENT_FOLDER
-
-upgrade-all-packages() {
-    local snapshot=""
-    parse_args "$@"
-
-    if [[ -z "$snapshot" ]]; then
-        echo -e "\033[1mDo you want to create a btrfs snapshot before upgrading?\033[0m"
-        echo "  1) Create snapshot and upgrade packages"
-        echo "  2) Only upgrade packages"
-        local choice
-        read -rp "Choose an option [1/2]: " choice
-        case "$choice" in
-            1) snapshot="true" ;;
-            *) snapshot="false" ;;
-        esac
-    fi
-
-    if [[ "$snapshot" == "true" ]]; then
-        echo "Creating btrfs snapshot..."
-        sudo snapper --config root create --type single --description "Pre-upgrade packages automatic" --cleanup-algorithm number
-    fi
-
-    echo "Upgrading all packages..."
-
-    echo -e "\033[1mPress any key to upgrade packages using \033[33mdnf\033[0m\033[1m...\033[0m"
-    read -n1 -s
-    sudo dnf upgrade --refresh
-
-    echo "----------------------------------------"
-    echo -e "\033[1mPress any key to upgrade packages using \033[33mpkcon\033[0m\033[1m...\033[0m"
-    read -n1 -s
-    pkcon refresh && pkcon update
-
-    echo "----------------------------------------"
-    echo -e "\033[1mPress any key to upgrade packages using \033[33mflatpak\033[0m\033[1m...\033[0m"
-    read -n1 -s
-    flatpak update
-
-    # echo "----------------------------------------"
-    # echo -e "\033[1mPress any key to upgrade packages using \033[33msnap\033[0m\033[1m...\033[0m"
-    # read -n1 -s
-    # sudo snap refresh
-
-    echo "----------------------------------------"
-    echo "Finishing package upgrades."
-}
-
 show-paths-env() {
     local pattern="${1:-}"
     local entries
